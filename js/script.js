ba3afac73d1614e662a1cfa315e2bbb273332648
rnggaul <raulll138138@gmail.com>
2024-09-12 11:58:16 +0700

    menambahkan shopping cart dan detail

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchButton = document.querySelector('#search-button')
     const searchForm = document.querySelector('.search-form');
     const searchBox = document.querySelector('#search-box');
+    // shopping cart
+    const shoppingCart = document.querySelector('.shopping-cart');
+    const shoppingButton = document.querySelector('#shopping-cart-button');
+    // Modal Box
+    const itemDetailModal = document.querySelector('#item-detail-modal');
+    const itemDetailButtons = document.querySelectorAll('.item-detail-button')
+    
 
     // toggle class active hamburger menu
     if (hamburgerMenu) {
@@ -23,6 +30,11 @@ document.addEventListener('DOMContentLoaded', function() {
         if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
             searchForm.classList.remove('active');
         }
+
+        if(!shoppingButton.contains(e.target) && !shoppingCart.contains(e.target)){
+            shoppingCart.classList.remove('active');
+        }
+
     });
     
     //toggle class active search form
@@ -31,5 +43,35 @@ document.addEventListener('DOMContentLoaded', function() {
         searchBox.focus();
         e.preventDefault();
     }
-        
+
+    // toggle class active shopping cart
+    if(shoppingButton) {
+        shoppingButton.onclick = (e) => {
+            shoppingCart.classList.toggle('active');
+            e.preventDefault();
+        }
+    }
+
+    //toggle class detail button
+    itemDetailButtons.forEach((btn) => {
+        if(btn) {
+            btn.onclick = (e) => {
+                itemDetailModal.style.display = 'flex';
+                e.preventDefault();
+            };
+        };
+    });
+
+    // close modal detail
+    document.querySelector('.modal .close-icon').onclick = (e) => {
+        itemDetailModal.style.display = 'none';
+        e.preventDefault();
+    }
+
+    // close di luar modal
+    window.onclick = (e) => {
+       if(e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+       }
+    }
 });
